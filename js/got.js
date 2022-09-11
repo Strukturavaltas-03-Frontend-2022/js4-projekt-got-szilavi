9322ed6cc5f62e7f10d7f6b416efcc14f2a7be27
k3csk3 <szilagyi.viktor89@gmail.com>
2022-09-11 22:30:16 +0200

    eddig jutottam

--- js/got.js
+++ js/got.js
@@ -38,6 +38,7 @@ fetch("./json/got.json")
       if (characterNameOnBoard === characters[i].name) {
         showCharacter(characters[i]);
         showCharacterImage();
+        e.currentTarget.children[e.currentTarget.children.length - 2];
       }
     }
   }
@@ -45,20 +46,20 @@ fetch("./json/got.json")
 
 function showCharacter(character) {
   const picture = document.querySelector(".characterinfo__container__img");
+  const houseOnBoard = document.querySelector(
+    ".characterinfo__container__house"
+  );
   const name = document.querySelector(".characterinfo__container__name");
   const characterInfo = document.querySelector(
     ".characterinfo__container__info"
   );
-  picture.src = character.picture;
+  picture.src =
+    character.picture ||
+    `https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png`;
   picture.alt = character.name;
-  name.textContent = character.name;
-  characterInfo.textContent = character.bio;
-
-  const houseName = character.house;
-  const houseOnBoard = document.querySelector(
-    ".characterinfo__container__house"
-  );
-  houseOnBoard.src = `assets/houses/${houseName}.png`;
+  name.textContent = character.name || "Name Not Found";
+  characterInfo.textContent = character.bio || "Bio Not Found";
+  houseOnBoard.src = `assets/houses/${character.house}.png`;
 }
 
 const input = document.querySelector(".searcher");
@@ -66,39 +67,84 @@ const input = document.querySelector(".searcher");
 input.addEventListener("keyup", searchCaracter);
 
 async function searchCaracter() {
+  claerAllInfo();
   let findCharacter = input.value.toUpperCase();
   let response = await fetch("./json/got.json").then((response) =>
     response.json()
   );
-
   response.forEach((i) => {
     if (findCharacter === i.name.toUpperCase()) {
       showCharacter(i);
+      showCharacterImage();
     }
   });
+
+  setTimeout(() => {
+    const characterName = document.querySelector(
+      ".characterinfo__container__name"
+    );
+
+    if (characterName.textContent === "" && findCharacter !== "") {
+      console.log(findCharacter);
+      characterName.textContent = `Character not found`;
+    }
+  }, 3000);
 }
 
 function showCharacterImage() {
   const characterImage = document.querySelector(
     ".characterinfo__container__img"
   );
-  console.log(characterImage.alt);
   if (characterImage.alt === "") {
-    console.log(characterImage.alt);
     characterImage.classList.add("hidden");
   } else {
     characterImage.classList.remove("hidden");
   }
 }
 
-showCharacterImage();
+function claerAllInfo() {
+  showCharacterImage();
+  const picture = document.querySelector(".characterinfo__container__img");
+  const name = document.querySelector(".characterinfo__container__name");
+  const characterInfo = document.querySelector(
+    ".characterinfo__container__info"
+  );
+  picture.src = "";
+  picture.alt = "";
+  name.textContent = "";
+  characterInfo.textContent = "";
+  const houseOnBoard = document.querySelector(
+    ".characterinfo__container__house"
+  );
+  houseOnBoard.src = ``;
+}
 
-//function resetInfo() {
-//  let charContainer = document.querySelector(".characterinfo__container");
-//  charContainer.classList.toggle("disabled");
-// if (
-//   charContainer.children[charContainer.children.length - 1].textContent === ""
-// ) {
-//   charContainer.classList.add("disabled");
-// }
-//};
+arrow = document.querySelector(".fa-angle-right");
+aside = document.querySelector(".search__container");
+header = document.querySelector(".header");
+searcher = document.querySelector(".searcher");
+searchIcon = document.querySelector(".fa-magnifying-glass");
+info = document.querySelector(".characterinfo__container");
+
+arrow.addEventListener("click", closeTab);
+
+function closeTab() {
+  aside.style.width === "8vh"
+    ? (aside.style.width = "30vw")
+    : (aside.style.width = "8vh");
+  header.style.visibility === "hidden"
+    ? (header.style.visibility = "visible")
+    : (header.style.visibility = "hidden");
+  searcher.style.visibility === "hidden"
+    ? (searcher.style.visibility = "visible")
+    : (searcher.style.visibility = "hidden");
+  searchIcon.style.visibility === "hidden"
+    ? (searchIcon.style.visibility = "visible")
+    : (searchIcon.style.visibility = "hidden");
+  info.style.visibility === "hidden"
+    ? (info.style.visibility = "visible")
+    : (info.style.visibility = "hidden");
+  arrow.classList.toggle("fa-angle-left");
+}
+
+showCharacterImage();
